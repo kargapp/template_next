239af3ff02502b8de194c854606612b3d4f44174
Karthik Gnanakumar <karthikgappiah@gmail.com>
2024-10-28 22:26:14 -0700

    fix: tailwind utility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/shadcn/ui/button";
+
 export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center">
-      <div>Hello World</div>
+      <Button variant={"default"}>Hello World</Button>
     </main>
   );
 }
